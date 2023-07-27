83db7f8979e29d2e0c40342c780e1c6843f2b08f
Prottoy Rasul <74609589+prottoyrasul@users.noreply.github.com>
2023-07-27 21:00:33 +0600

    Update whatsappmessageautomation.py

--- whatsappmessageautomation.py
+++ whatsappmessageautomation.py
@@ -1,6 +1,6 @@
 #enter the terminal in vs code and write :    pip install pywhatkit
-then the terminal installing whatkit
-after thaty write the code:
+#then the terminal installing whatkit
+#after thaty write the code:
 
 import pywhatkit
 pywhatkit.sendwhatmsg('+8801797990054', 'Hi hello', 20, 13)
